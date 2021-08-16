32b545c45c6330ea9ea155807ef6cc2d294e2e95
Stephanie <whatawhat@hotmail.com>
2021-08-16 13:11:21 -0400

    imported useDispatch and useSelector

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,6 +5,7 @@ import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
+import { useDispatch, useSelector } from 'react-redux';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
 import './style.css';
